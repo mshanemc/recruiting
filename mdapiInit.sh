b083a24345b70984122c553a2d6066303ec3fa0e
Shane McLaughlin <shane.mclaughlin@salesforce.com>
2018-03-01 22:01:56 -0600

    cleanup stuff we don't do since DCP

--- mdapiInit.sh
+++
@@ -1,8 +0,0 @@
-sfdx force:org:create -s -f config/project-scratch-def.json -d 1
-sfdx force:mdapi:deploy -f mdapi.zip -w 20
-
-sfdx force:user:permset:assign -n Referrals
-sfdx force:data:bulk:upsert -f data/referrals.csv -s Referral__c -i Id
-
-sfdx force:org:open
-
